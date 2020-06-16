6aec67480b8a860394a08871fab27672dba2fa99
一杯柠檬水 <lgf768561@163.com>
2020-06-16 17:13:15 +0800

    代码

---
+++ pytorch-imageclassification/AlexNetTest/test/predict.py
@@ -0,0 +1,44 @@
+# Author LGF
+# GreatTime 2020/5/30
+# Description: simple introduction of the code
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import json
+from AlexNetTest.test.model import AlexNet
+
+data_transform = transforms.Compose(
+    [transforms.Resize((224, 224)),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# load image
+img = Image.open('../pic/1.jpg')
+plt.imshow(img)
+# [N, C, H, W]
+img = data_transform(img)
+# expand batch dimension
+img = torch.unsqueeze(img, dim=0)
+
+# read class_indict
+try:
+    json_file = open('./class_indices.json', 'r')
+    class_indict = json.load(json_file)
+except Exception as e:
+    print(e)
+    exit(-1)
+
+# create model
+model = AlexNet(num_classes=22)
+# load model weights
+model_weight_path = "./AlexNet.pth"
+model.load_state_dict(torch.load(model_weight_path))
+model.eval()
+with torch.no_grad():
+    # predict class
+    output = torch.squeeze(model(img))
+    predict = torch.softmax(output, dim=0)
+    predict_cla = torch.argmax(predict).numpy()
+print(class_indict[str(predict_cla)], predict[predict_cla].item())
+plt.show()
---
+++ pytorch-imageclassification/AlexNetTest/test/train.py
@@ -0,0 +1,98 @@
+# Author LGF
+# GreatTime 2020/5/30
+# Description: simple introduction of the code
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets, utils
+import torch.optim as optim
+import json
+import time
+from AlexNetTest.test.model import AlexNet
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+data_transform = {
+    "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
+    "val": transforms.Compose([transforms.Resize((224, 224)),  # cannot 224, must (224, 224)
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
+
+image_path = "../data_set/image_data/"  # flower data set path
+train_dataset = datasets.ImageFolder(root=image_path + "/train",
+                                     transform=data_transform["train"])
+train_num = len(train_dataset)
+
+flower_list = train_dataset.class_to_idx
+cla_dict = dict((val, key) for key, val in flower_list.items())
+# write dict into json file
+json_str = json.dumps(cla_dict, indent=4)
+with open('class_indices.json', 'w') as json_file:
+    json_file.write(json_str)
+
+batch_size = 32
+train_loader = torch.utils.data.DataLoader(train_dataset,
+                                           batch_size=batch_size, shuffle=True,
+                                           num_workers=0)
+
+validate_dataset = datasets.ImageFolder(root=image_path + "/val",
+                                        transform=data_transform["val"])
+val_num = len(validate_dataset)
+validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                              batch_size=batch_size, shuffle=True,
+                                              num_workers=0)
+
+
+net = AlexNet(num_classes=22, init_weights=True)
+
+net.to(device)
+loss_function = nn.CrossEntropyLoss()
+# pata = list(net.parameters())
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
+
+save_path = './AlexNet.pth'
+best_acc = 0.0
+for epoch in range(100):
+    # train
+    net.train()
+    running_loss = 0.0
+    t1 = time.perf_counter()
+    for step, data in enumerate(train_loader, start=0):
+        images, labels = data
+        optimizer.zero_grad()
+        outputs = net(images.to(device))
+        loss = loss_function(outputs, labels.to(device))
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        # print train process
+        rate = (step + 1) / len(train_loader)
+        a = "*" * int(rate * 50)
+        b = "." * int((1 - rate) * 50)
+        print("\rtrain loss: {:^3.0f}%[{}->{}]{:.3f}".format(int(rate * 100), a, b, loss), end="")
+    print()
+    print(time.perf_counter()-t1)
+
+    # validate
+    net.eval()
+    acc = 0.0  # accumulate accurate number / epoch
+    with torch.no_grad():
+        for val_data in validate_loader:
+            val_images, val_labels = val_data
+            outputs = net(val_images.to(device))
+            predict_y = torch.max(outputs, dim=1)[1]
+            acc += (predict_y == val_labels.to(device)).sum().item()
+        val_accurate = acc / val_num
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+        print('[epoch %d] train_loss: %.3f  test_accuracy: %.3f' %
+              (epoch + 1, running_loss / step, val_accurate))
+
+print('Finished Training')
